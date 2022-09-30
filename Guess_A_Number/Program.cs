228c4b3bfe833a5c549eeebd8a9d766b434719de
BozhidarYordanov54 <108609381+BozhidarYordanov54@users.noreply.github.com>
2022-09-30 17:08:40 +0300

    Add files via upload

---
+++ Guess_A_Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Guess_A_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Guess A Number (1 - 100): ");
+            int number = int.Parse(Console.ReadLine());
+
+            Random randomNumber = new Random();
+            int rndNum = randomNumber.Next(1, 100);
+
+            while (rndNum != number)
+            {
+                if (rndNum > number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.Write("Guess A Number (1 - 100): ");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You guessed it!");
+        }
+    }
+}
